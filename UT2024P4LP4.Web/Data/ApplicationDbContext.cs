ac9de46fc4bb3cdcbd79018fcf5452d4aafbe475
Felix Benzan <felixbenz07@gmail.com>
2024-10-15 21:05:21 -0400

    3ero Agregamos al AppDbContext nuestra nueva tabla Productos y de paso el SaveChange con la interfaz

--- UT2024P4LP4.Web/Data/ApplicationDbContext.cs
+++ UT2024P4LP4.Web/Data/ApplicationDbContext.cs
@@ -1,9 +1,15 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using UT2024P4LP4.Web.Data.Entities;
 
-namespace UT2024P4LP4.Web.Data
+namespace UT2024P4LP4.Web.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options), IApplicationDbContext
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    public DbSet<Producto> Productos { get; set; }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
+        return base.SaveChangesAsync(cancellationToken);
     }
 }
---
+++ UT2024P4LP4.Web/Data/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UT2024P4LP4.Web.Data.Entities;
+
+namespace UT2024P4LP4.Web.Data
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Producto> Productos { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
