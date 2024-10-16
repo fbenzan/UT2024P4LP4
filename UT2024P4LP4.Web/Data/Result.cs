4d4e9d9c6b482cb5f5c3e558832935b34d9a8e23
Felix Benzan <felixbenz07@gmail.com>
2024-10-15 21:04:01 -0400

    1ero crear la clases para resultados de trabnsacciones

---
+++ UT2024P4LP4.Web/Data/Result.cs
@@ -0,0 +1,53 @@
+﻿namespace UT2024P4LP4.Web.Data;
+/// <summary>
+/// Resultado simple
+/// </summary>
+public class Result
+{
+    public bool Succesd { get; set; }
+    public string Message { get; set; } = "";
+
+    public static Result Success(string message)
+    => new()
+    {
+        Succesd = true,
+        Message = message
+    };
+    public static Result Failure(string message)
+    => new()
+    {
+        Message = message
+    };
+}
+public class Result<T> : Result
+{
+    public T? Data { get; set; }
+    public static Result<T> Success(T data,string message = "Ok")
+    => new()
+    {
+        Data = data,
+        Succesd = true,
+        Message = message
+    };
+    public static Result<T> Failure(string message)
+    => new()
+    {
+        Message = message
+    };
+}
+public class ResultList<T> : Result
+{
+    public ICollection<T>? Data { get; set; }
+    public static ResultList<T> Success(ICollection<T> data, string message = "Ok")
+    => new()
+    {
+        Data = data,
+        Succesd = true,
+        Message = message
+    };
+    public static ResultList<T> Failure(string message)
+    => new()
+    {
+        Message = message
+    };
+}
